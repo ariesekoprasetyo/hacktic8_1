c7dd099d894fcb58af3ea75c89250bb4ad6e43b1
mongol3 <ariesekoprasetyo@outlook.com>
2023-04-20 05:00:54 +0700

    [REFACTOR] Refactor Code

--- main.go
+++ main.go
@@ -7,12 +7,23 @@ import (
 )
 
 func main() {
-	var namaSearch = models.SearchData(models.Name)
+	// Dummy Data
+	dataSiswa := []models.Siswa{
+		{ID: 1, Nama: "Aldi", Alamat: "Jakarta", Pekerjaan: "Karyawan", Alasan: "Karena ingin meningkatkan skill programming"},
+		{ID: 2, Nama: "Tashya", Alamat: "Jakarta", Pekerjaan: "Mahasiswa", Alasan: "Karena ingin membuat aplikasi yang lebih efisien"},
+		{ID: 3, Nama: "Ihza", Alamat: "Jakarta", Pekerjaan: "Programmer", Alasan: "Karena ingin mengembangkan aplikasi berperforma tinggi\""},
+		{ID: 4, Nama: "Syarif", Alamat: "Jakarta", Pekerjaan: "Karyawan", Alasan: "Karena ingin mempelajari bahasa pemrograman yang baru"},
+		{ID: 5, Nama: "Valdo", Alamat: "Jakarta", Pekerjaan: "Karyawan", Alasan: "Karena ingin meningkatkan kesempatan karir di bidang IT"},
+		{ID: 6, Nama: "Husin", Alamat: "Jakarta", Pekerjaan: "Backend Developer", Alasan: "Karena tertarik dengan fitur-fitur bahasa Go"},
+		{ID: 7, Nama: "Aries", Alamat: "Bekasi", Pekerjaan: "Karyawan Swasta", Alasan: "Karena Go merupakan bahasa pemrograman yang mudah dipelajari"},
+	}
+
+	getByName := models.SearchData(dataSiswa)
 	if len(os.Args) != 2 {
 		fmt.Println("Masukkan Hanya 1 Parameter")
 		return
 	}
-	result := namaSearch(os.Args[1])
+	result := getByName(os.Args[1])
 	if result.ID == 0 {
 		fmt.Println("Data Tidak Ada")
 		return
--- models/person.go
+++ models/person.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-type DataSiswa struct {
+type Siswa struct {
 	ID        int
 	Nama      string
 	Alamat    string
@@ -12,23 +12,13 @@ type DataSiswa struct {
 	Alasan    string
 }
 
-var Name = []DataSiswa{
-	{ID: 1, Nama: "Aldi", Alamat: "Jakarta", Pekerjaan: "Karyawan", Alasan: "Karena ingin meningkatkan skill programming"},
-	{ID: 2, Nama: "Tashya", Alamat: "Jakarta", Pekerjaan: "Mahasiswa", Alasan: "Karena ingin membuat aplikasi yang lebih efisien"},
-	{ID: 3, Nama: "Ihza", Alamat: "Jakarta", Pekerjaan: "Programmer", Alasan: "Karena ingin mengembangkan aplikasi berperforma tinggi\""},
-	{ID: 4, Nama: "Syarif", Alamat: "Jakarta", Pekerjaan: "Karyawan", Alasan: "Karena ingin mempelajari bahasa pemrograman yang baru"},
-	{ID: 5, Nama: "Valdo", Alamat: "Jakarta", Pekerjaan: "Karyawan", Alasan: "Karena ingin meningkatkan kesempatan karir di bidang IT"},
-	{ID: 6, Nama: "Husin", Alamat: "Jakarta", Pekerjaan: "Wiraswasta", Alasan: "Karena tertarik dengan fitur-fitur bahasa Go"},
-	{ID: 7, Nama: "Aries", Alamat: "Jakarta", Pekerjaan: "Wiraswasta", Alasan: "Karena Go merupakan bahasa pemrograman yang mudah dipelajari"},
-}
-
-func SearchData(dataSiswa []DataSiswa) func(string) DataSiswa {
-	return func(nama string) DataSiswa {
+func SearchData(dataSiswa []Siswa) func(string) Siswa {
+	return func(nama string) Siswa {
 		for _, v := range dataSiswa {
-			if strings.ToLower(v.Nama) == strings.ToLower(nama) {
+			if strings.EqualFold(v.Nama, nama) {
 				return v
 			}
 		}
-		return DataSiswa{}
+		return Siswa{}
 	}
 }
